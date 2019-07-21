1df3919bbbcf11abe1c5509e101e82ead318a56a
TakakiMiyajima <takaki.mz@gmail.com>
2019-07-21 18:28:40 +0900

    feat: 石を置く場所によって、裏返すことができる相手の石があるかどうかをチェックするためのメソッドを追加する

--- othello.py
+++ othello.py
@@ -22,7 +22,7 @@ class OthelloBoard(object):
 
     def put_disk(self, x, y, player):
 
-        """指定した座標に指定したプレイヤーの石を置く
+        """　指定した座標に指定したプレイヤーの石を置く
         Args:
             x: 置く石のX座標
             y: 置く石のY座標
@@ -37,21 +37,111 @@ class OthelloBoard(object):
         """
 
         # 既にほかの石があれば置くことができない
-        # if self.cells[y][x] is not None:
-        #     return False
+        if self.cells[y][x] is not None:
+            return False
 
         # # 獲得できる石がない場合も置くことができない
-        # flippable = self.list_flippable_disks(x, y, player)
-        # if not flippable:
-        #     return False
+        flippable = self.list_flippable_disks(x, y, player)
+        if not flippable:
+            return False
 
         # # 実際に石を置く処理
-        # self.cells[y][x] = player
-        # for x,y in flippable:
-        #     self.cells[y][x] = player
+        self.cells[y][x] = player
+        for x,y in flippable:
+            self.cells[y][x] = player
 
-        # return True
+        return True
+
+    def list_flippable_disks(self, x, y, player):
+
+        """　指定した座標に指定したプレイヤーの石を置いた時、ひっくりかえせる全ての石の座標（タプル）をリストにして返す
+        Args:
+            x: x座標
+            y: y座標
+            player: 石を置こうとしているプレイヤー
+
+        Returns:
+            ひっくりかえすことができる全ての石の座標（タプル）のリスト
+            または空リスト
+        """
+
+        PREV = -1
+        NEXT = 1
+        DIRECTION = [PREV, 0, NEXT]
+        flippable = []
+
+        for dx in DIRECTION:
+            for dy in DIRECTION:
+                if dx == 0 and dy == 0:
+                    continue
+
+                tmp = []
+                depth = 0
+
+                while(True):
+                    depth += 1
+
+                    # 方向 × 深さ(距離)を要求座標に加算し直線的な探査をする
+                    rx = x + (dx * depth)
+                    ry = y + (dy * depth)
+
+                    # 調べる座標(rx, ry)がボードの範囲内ならば
+                    if 0 <= rx < BOARD_SIZE and 0 <= ry < BOARD_SIZE:
+                        request = self.cells[ry][rx]
+
+                        # Noneを獲得することはできない
+                        if request is None:
+                            break
+
+                        if request == player:  # 自分の石が見つかったとき
+                            if tmp != []:      # 探査した範囲内に獲得可能な石があれば
+                                flippable.extend(tmp) # flippableに追加
+                        # 相手の石が見つかったとき
+                        else:
+                            # 獲得可能な石として一時保存
+                            tmp.append((rx, ry))
+
+                    else:
+                        break
+        return flippable
+
+    def show_board(self):
+        """ボードを表示する"""
+        print("--" * 20)
+        for i in self.cells:
+            for cell in i:
+                if cell == WHITE:
+                    print("W", end=" ")
+                elif cell == BLACK:
+                    print("B", end=" ")
+                else:
+                    print("*", end=" ")
+            print("\n", end="")
+
+    def list_possible_cells(self, player):
+        """指定したプレイやーの石を置くことができる、すべてのマスの座標をリストにして返す
+        Args:
+            player: 石を置こうとしているプレイヤー
+
+        Returns:
+            石を置くことができるマスの座標のリスト
+            または空リスト
+        """
+
+        possible = []
+        for x in range(BOARD_SIZE):
+            for y in range(BOARD_SIZE):
+                if self.cells[y][x] is not None:
+                    continue
+                if self.list_flippable_disks(x, y, player) == []:
+                    continue
+                else:
+                    possible.append((x, y))
+        return possible
 
 
-board = OthelloBoard()
-print(board.cells)
+if __name__ == '__main__':
+    board = OthelloBoard()
+    board.show_board()
+    board.put_disk(3, 2, BLACK)
+    board.show_board()
